7e1801d2869683b03ddabca17db87b777c349453
NarciSource <jwch11@gmail.com>
2024-08-27 18:27:05 +0900

    chore: graphql 파일 업로드 준비

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import http from 'http';
 import 'reflect-metadata';
 import cookieParser from 'cookie-parser';
+import { graphqlUploadExpress } from "graphql-upload";
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ async function main() {
         res.setHeader('Referrer-Policy', 'no-referrer');
         next();
     }, cookieParser()); // cookie-parser 미들웨어 추가
+
+    app.use(express.static("public")); // 'public' 폴더를 정적 파일 제공 폴더로 설정
+    app.use(graphqlUploadExpress({ maxFileSize: 1024 * 1000 * 5, maxFiles: 1 })); // graphql-upload 미들웨어 추가
 
     app.get("/", (req, res) => {
         res.status(200).send(); // for healthcheck
