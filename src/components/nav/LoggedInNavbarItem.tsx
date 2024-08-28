e74b0d72db5e293e5c51b9689397243ce37c1cc8
NarciSource <jwch11@gmail.com>
2024-08-28 18:21:56 +0900

    feat: 프로필 이미지 데이터 주입

--- src/components/nav/LoggedInNavbarItem.tsx
+++ src/components/nav/LoggedInNavbarItem.tsx
@@ -1,11 +1,29 @@
-import { Avatar, Menu, MenuButton, MenuItem, MenuList, Stack } from '@chakra-ui/react';
+import { ChangeEvent } from 'react';
+import { Avatar, Text, Box, Flex, Menu, MenuButton, MenuItem, MenuList, Stack } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
-import { useLogoutMutation } from '../../generated/graphql';
+import { useLogoutMutation, useMeQuery, useUploadProfileImageMutation } from '../../generated/graphql';
 import { useApolloClient } from '@apollo/client';
 
 export default function LoggedInNavbarItem(): React.ReactElement {
     const [logout, { loading: logoutLoading }] = useLogoutMutation();
     const client = useApolloClient();
+
+    const accessToken = localStorage["access_token"];
+    const { data: userData } = useMeQuery({ skip: !accessToken });
+
+    const [upload] = useUploadProfileImageMutation();
+    const profileImageSrc = `${process.env.REACT_APP_API_HOST}/${userData?.me?.profileImage}`;
+
+    async function handleImageUpload(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.files) {
+            const file = e.target.files[0];
+
+            await upload({
+                variables: { file },
+                update: (cache) => cache.evict({ fieldName: "me" }),
+            });
+        }
+    }
 
     async function onLogoutClick() {
         try {
@@ -25,9 +43,20 @@ export default function LoggedInNavbarItem(): React.ReactElement {
 
             <Menu>
                 <MenuButton>
-                    <Avatar size="sm" />
+                    <Avatar size="sm" src={profileImageSrc} />
                 </MenuButton>
                 <MenuList>
+                    <Flex px={4} pt={2} pb={4}>
+                        <label htmlFor="upload-profile-image">
+                            <input id="upload-profile-image" type="file" accept="image/*" hidden onChange={handleImageUpload} />
+                            <Avatar size="md" src={profileImageSrc} mr={4} cursor="pointer" />
+                        </label>
+
+                        <Box>
+                            <Text fontWeight="bold">{userData?.me?.username}</Text>
+                            <Text>{userData?.me?.email}</Text>
+                        </Box>
+                    </Flex>
                     <MenuItem onClick={onLogoutClick} isDisabled={logoutLoading}>
                         로그아웃
                     </MenuItem>
