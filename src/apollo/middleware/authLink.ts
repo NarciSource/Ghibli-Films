f94c740f13b763005b9bacec5f91a5ddbffe0fac
NarciSource <jwch11@gmail.com>
2025-11-08 19:55:41 +0900

    fix: 엑세스토큰 접근 수정

--- src/apollo/middleware/authLink.ts
+++ src/apollo/middleware/authLink.ts
@@ -1,7 +1,9 @@
 import { setContext } from '@apollo/client/link/context';
 
 const authLink = setContext((_request, prevContext: { headers?: Record<string, string> }) => {
-  const accessToken = localStorage.getItem('access_token');
+  const auth = localStorage.getItem('auth');
+  const accessToken = auth ? JSON.parse(auth).state.accessToken : null;
+
   return {
     headers: {
       ...prevContext.headers,
