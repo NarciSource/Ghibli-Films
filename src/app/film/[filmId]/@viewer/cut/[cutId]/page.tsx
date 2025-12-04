60df9aeda7c0576c75e4ce4fec9264c860b09f30
NarciSource <jwch11@gmail.com>
2025-12-04 21:50:12 +0900

    fix: 명장면 데이터 연결 오류 수정

--- src/app/film/[filmId]/@viewer/cut/[cutId]/page.tsx
+++ src/app/film/[filmId]/@viewer/cut/[cutId]/page.tsx
@@ -6,19 +6,29 @@ import type { CutsQuery } from '@/graphql/api/operations';
 import CutModal from '../../_components/CutModal';
 import CutSlide from '../../_components/CutSlide';
 
-export default async function Page({ params }: { params: Promise<{ cutId: string }> }) {
-  const { cutId } = await params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ filmId: string; cutId: string }>;
+}) {
+  const { filmId, cutId } = await params;
   const apolloClient = await createApolloClient({});
 
   try {
     const { data } = await apolloClient.query<CutsQuery>({
       query: CutsDocument,
-      variables: { filmId: Number(cutId) },
+      variables: { filmId: Number(filmId) },
     });
+
+    const page = data.cuts.findIndex((cut) => cut.id === Number(cutId));
+
+    if (page === -1) {
+      return <Text>존재하지 않는 장면입니다.</Text>;
+    }
 
     return (
       <CutModal>
-        <CutSlide items={data.cuts} page={Number(cutId)} />
+        <CutSlide items={data.cuts} page={page} />
       </CutModal>
     );
   } catch (error) {
