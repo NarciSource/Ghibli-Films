1a1365a751263c7b49d760ae185ac2d33b659c4a
NarciSource <jwch11@gmail.com>
2025-12-06 23:15:45 +0900

    fix: Carousel 전환 시 layout shift 플래시 현상 해결
    - layout으로 상위 안정적인 계층으로 옮겨 Carousel 영역이 unmount되지 않도록 조정

--- src/app/film/[filmId]/@viewer/(.)cut/[cutId]/page.tsx
+++ src/app/film/[filmId]/@viewer/(.)cut/[cutId]/page.tsx
@@ -1,35 +1,9 @@
-'use client';
-
-import { useParams, useRouter, useSearchParams } from 'next/navigation';
-import { Text } from '@chakra-ui/react';
-
-import { useCutsStore } from '../../../_store/useCutsStore';
 import CutDetail from '../../_components/CutDetail';
-import CutModal from './_components/CutModal';
-import CutSlide from './_components/CutSlide';
 
-export default function Page() {
-  const { cutId } = useParams();
-  const nth = useSearchParams().get('nth');
-  const navigate = useRouter();
+type PageParams = Promise<{ cutId: string }>;
 
-  const { cuts } = useCutsStore.getState();
+export default async function Page({ params }: { params: PageParams }) {
+  const { cutId } = await params;
 
-  const onPageChange = ({ page }: { page: number }) => {
-    navigate.replace(`./${cuts[page].id}?nth=${page}`);
-  };
-
-  try {
-    return (
-      <CutModal>
-        <CutSlide slideCount={cuts.length} defaultPage={Number(nth)} onPageChange={onPageChange}>
-          <CutDetail cutId={Number(cutId)} />
-        </CutSlide>
-      </CutModal>
-    );
-  } catch (error) {
-    console.error(error);
-
-    return <Text>영화 장면을 표시할 수 없습니다.</Text>;
-  }
+  return <CutDetail cutId={Number(cutId)} />;
 }
