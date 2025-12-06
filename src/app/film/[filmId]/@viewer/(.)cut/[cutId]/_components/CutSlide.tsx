d1c96a7fdb0999d8636c89e0f5c9f72e0b43efe4
NarciSource <jwch11@gmail.com>
2025-12-06 21:55:36 +0900

    feat: 슬라이드 이동 시 파라미터 업데이트

--- src/app/film/[filmId]/@viewer/(.)cut/[cutId]/_components/CutSlide.tsx
+++ src/app/film/[filmId]/@viewer/(.)cut/[cutId]/_components/CutSlide.tsx
@@ -1,29 +1,83 @@
+import { forwardRef } from 'react';
+import { LuArrowLeft, LuArrowRight } from 'react-icons/lu';
 import { v4 as uuidv4 } from 'uuid';
-import { Carousel, For } from '@chakra-ui/react';
+import {
+  Carousel,
+  type CarouselRootProps,
+  For,
+  IconButton,
+  type IconButtonProps,
+} from '@chakra-ui/react';
 
-import CutSlideOverlay from './CutSlideOverlay';
 import LazySlide from './LazySlide';
 
 export default function CutSlide({
-  itemSize,
-  page,
   children,
-}: {
-  itemSize: number;
-  page: number;
-  children: (id: number) => React.ReactNode;
+  ...props
+}: CarouselRootProps & {
+  children: React.ReactNode;
 }) {
+  const actionButtonStyle = {
+    top: '50%',
+    transform: 'auto',
+    translateY: 'calc(-1 * var(--chakra-spacing-32))',
+  };
+
   return (
-    <CutSlideOverlay slideCount={itemSize} defaultPage={page}>
-      <Carousel.ItemGroup>
-        <For each={Array(itemSize).fill(0)}>
-          {(_, index) => (
-            <Carousel.Item key={uuidv4()} index={index}>
-              <LazySlide index={index}>{children(index)}</LazySlide>
-            </Carousel.Item>
-          )}
-        </For>
-      </Carousel.ItemGroup>
-    </CutSlideOverlay>
+    <Carousel.Root {...props}>
+      <Carousel.Control justifyContent='center' gap='4'>
+        <Carousel.PrevTrigger asChild>
+          <ActionButton direction='left' {...actionButtonStyle}>
+            <LuArrowLeft />
+          </ActionButton>
+        </Carousel.PrevTrigger>
+
+        <Carousel.ItemGroup>
+          <For each={Array(props.slideCount).fill(0)}>
+            {(_, index) => (
+              <Carousel.Item key={uuidv4()} index={index}>
+                <LazySlide index={index}>{children}</LazySlide>
+              </Carousel.Item>
+            )}
+          </For>
+        </Carousel.ItemGroup>
+
+        <Carousel.NextTrigger asChild>
+          <ActionButton direction='right' {...actionButtonStyle}>
+            <LuArrowRight />
+          </ActionButton>
+        </Carousel.NextTrigger>
+      </Carousel.Control>
+    </Carousel.Root>
   );
 }
+
+interface ActionButtonProps extends IconButtonProps {
+  direction?: 'left' | 'right';
+}
+
+const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(function ActionButton(
+  { direction, ...props },
+  ref,
+) {
+  const insetMap = {
+    left: { insetStart: 4 },
+    right: { insetEnd: 4 },
+  } as const;
+
+  const inset = direction ? (insetMap[direction] ?? {}) : {};
+
+  return (
+    <IconButton
+      {...inset}
+      {...props}
+      ref={ref}
+      size='xs'
+      variant='outline'
+      rounded='full'
+      position='absolute'
+      zIndex='1'
+      bg='bg'
+    />
+  );
+});
--- src/app/film/[filmId]/@viewer/(.)cut/[cutId]/page.tsx
+++ src/app/film/[filmId]/@viewer/(.)cut/[cutId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import { Text } from '@chakra-ui/react';
 
 import { useCutsStore } from '../../../_store/useCutsStore';
@@ -9,15 +9,21 @@ import CutModal from './_components/CutModal';
 import CutSlide from './_components/CutSlide';
 
 export default function Page() {
+  const { cutId } = useParams();
   const nth = useSearchParams().get('nth');
+  const navigate = useRouter();
 
   const { cuts } = useCutsStore.getState();
+
+  const onPageChange = ({ page }: { page: number }) => {
+    navigate.replace(`./${cuts[page].id}?nth=${page}`);
+  };
 
   try {
     return (
       <CutModal>
-        <CutSlide itemSize={cuts.length} page={Number(nth)}>
-          {(nth) => <CutDetail cutId={cuts[nth].id} />}
+        <CutSlide slideCount={cuts.length} defaultPage={Number(nth)} onPageChange={onPageChange}>
+          <CutDetail cutId={Number(cutId)} />
         </CutSlide>
       </CutModal>
     );
