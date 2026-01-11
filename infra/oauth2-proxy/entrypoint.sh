af35e8b641b75065666d700a7b1c1f771a9498ca
NarciSource <jwch11@gmail.com>
2026-01-11 19:25:42 +0900

    refactor: Keycloak secret을 동적 조회

---
+++ infra/oauth2-proxy/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# 패키지 설치
+apk add --no-cache curl jq
+
+# Admin token 발급
+TOKEN=$(curl -s -X POST "http://gateway:80/auth/realms/master/protocol/openid-connect/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "username=${KEYCLOAK_ADMIN}" \
+  -d "password=${KEYCLOAK_ADMIN_PASSWORD}" \
+  -d "grant_type=password" \
+  -d "client_id=admin-cli" | jq -r '.access_token')
+
+# client UUID 조회
+CLIENT_UUID=$(curl -s -X GET "http://gateway:80/auth/admin/realms/dev/clients?clientId=${OAUTH2_PROXY_CLIENT_ID}" \
+  -H "Authorization: Bearer $TOKEN" | jq -r '.[0].id')
+echo "CLIENT_UUID: $CLIENT_UUID"
+
+# client-secret 조회
+CLIENT_SECRET=$(curl -s -X GET "http://gateway:80/auth/admin/realms/dev/clients/$CLIENT_UUID/client-secret" \
+  -H "Authorization: Bearer $TOKEN" | jq -r '.value')
+echo "CLIENT_SECRET: $CLIENT_SECRET"
+
+# 시크릿 환경변수 설정
+export OAUTH2_PROXY_CLIENT_SECRET=$CLIENT_SECRET
+
+# oauth2-proxy 시작
+exec /bin/oauth2-proxy --config=/etc/oauth2-proxy/config.cfg
