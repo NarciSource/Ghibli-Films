8bea943f56a9b1ff6954d4226ddcfb8eb6f998cc
NarciSource <jwch11@gmail.com>
2026-01-24 18:55:58 +0900

    chore(docker): 도커 컴포즈 정리

--- infra/oauth2-proxy/entrypoint.sh
+++ infra/oauth2-proxy/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 apk add --no-cache curl jq
 
 # Admin token 발급
-TOKEN=$(curl -s -X POST "http://gateway:80/auth/realms/master/protocol/openid-connect/token" \
+TOKEN=$(curl -s -X POST "http://traefik:80/auth/realms/master/protocol/openid-connect/token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "username=${KEYCLOAK_ADMIN}" \
   -d "password=${KEYCLOAK_ADMIN_PASSWORD}" \
@@ -13,12 +13,12 @@ TOKEN=$(curl -s -X POST "http://gateway:80/auth/realms/master/protocol/openid-co
   -d "client_id=admin-cli" | jq -r '.access_token')
 
 # client UUID 조회
-CLIENT_UUID=$(curl -s -X GET "http://gateway:80/auth/admin/realms/dev/clients?clientId=${OAUTH2_PROXY_CLIENT_ID}" \
+CLIENT_UUID=$(curl -s -X GET "http://traefik:80/auth/admin/realms/dev/clients?clientId=${OAUTH2_PROXY_CLIENT_ID}" \
   -H "Authorization: Bearer $TOKEN" | jq -r '.[0].id')
 echo "CLIENT_UUID: $CLIENT_UUID"
 
 # client-secret 조회
-CLIENT_SECRET=$(curl -s -X GET "http://gateway:80/auth/admin/realms/dev/clients/$CLIENT_UUID/client-secret" \
+CLIENT_SECRET=$(curl -s -X GET "http://traefik:80/auth/admin/realms/dev/clients/$CLIENT_UUID/client-secret" \
   -H "Authorization: Bearer $TOKEN" | jq -r '.value')
 echo "CLIENT_SECRET: $CLIENT_SECRET"
 
